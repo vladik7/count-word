7143c4ad183c1de49723aff25a2de716b2fa1325
Alano13 <alan@xaker.ru>
2016-04-17 18:51:43 +0300

    Added fix requests.

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿// невесть откуда взявшиеся пустые строки.
 
 namespace ConsoleApplication2
 {
@@ -6,6 +6,8 @@ namespace ConsoleApplication2
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.IO;
+
+    // не использущиеся нэймеспейсы.
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -15,28 +17,46 @@ namespace ConsoleApplication2
     /// </summary>
     public class Program
     {
+        // Требования заккоментировать заголовки - единственные требования, которые можно смело игнорировать.
+
         /// <summary>
         /// The main.
         /// </summary>
         /// <param name="args">
         /// The args.
         /// </param>
+
+        // Вот эти два атрибута (В квадратных скобках, это как раз атрибуты. Т.е. данные, дополнительно описывающие класс) нахрен никому не нужны.
+        // Они отменяют предупрежедние об ошибках, которые уже исправлены.
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1303:ConstFieldNamesMustBeginWithUpperCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1306:FieldNamesMustBeginWithLowerCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
+
+        // не использующийся аргумент метода.
         public static void Main(string[] args)
         {
+            // вместо самопальной строки можно например использовать такой метод как char.IsLetterOrDigit().
             const string Delay = ".,:; !?";
+
+            // Сокращения не приветствуются, если они не общепринятые. Не экономь на именах переменных
             var ans = new HashSet<string>();
+
+            // возможность указать имена файлов добавь тогда уж. В отдельную функцию там вынеси что ли.
             for (var k = 1; k <= 3; k++)
             {
+                 
                 var path = @"d:\" + k + ".txt";
 
+                // маленькие файлы можно загружать в память целиком, с помощью например File.ReadAllText. Хотя канеш построчное чтение универсальнее.
                 using (var sr = File.OpenText(path))
                 {
                     string s;
                     while ((s = sr.ReadLine()) != null)
                     {
                         var word = string.Empty;
+
+                        // Охрененно тяжело понять, что же происходит в цикле, не глядя на строки выше.
+                        // Однобуквенные имена подходят там и только там, где они уместны по контексту.
+                        // Например переменную t какого-нибудь уравнения можно назвать t
                         foreach (var t in s)
                         {
                             if (Delay.IndexOf(t) < 0)
@@ -61,6 +81,8 @@ namespace ConsoleApplication2
                         }
 
                         ans.Add(word);
+
+                        // вообще лишняя строка
                         word = string.Empty;
                     }
                 }
